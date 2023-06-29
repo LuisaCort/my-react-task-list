ab65b735311ecdf6b3c6c7a48757b08003d1c000
LuisaCort <luisacortes.ingbio@gmail.com>
2023-06-29 15:50:18 -0500

    fixed some behaviors

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -5,18 +5,26 @@ const Task = (props) => {
   const {id, onDelete, onEdit} = props
   const [input, setInput] = useState(props.desc)
   const [state, setState] = useState(props.state)
+  const [height, setHeight] = useState("30px")
   const handleInput = item => setInput(item.target.value)
   const handleState = item => setState(item.target.checked)
   const handleDelete = () => onDelete(id)
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => onEdit(id, input, state), [input, state])
+  const handleFullSize = event => setHeight(event.currentTarget.scrollHeight+"px")
+  const handleSmallSize = () => setHeight("30px")
   return(
     <div id="Task">
       <input type="checkbox" checked={state} onChange={handleState}/>
-      <input type="text" value={input} onChange={handleInput} disabled={state}/>
-      <div id="ButtonsContainer">
-        <button onClick={handleDelete}>🗑️</button>
-      </div>
+      <textarea 
+        value={input} 
+        onChange={handleInput} 
+        disabled={state} 
+        style={{height:height}}
+        onFocus={handleFullSize} 
+        onBlur={handleSmallSize}
+      />
+      <button onClick={handleDelete}>🗑️</button>
     </div>
   )
 }
